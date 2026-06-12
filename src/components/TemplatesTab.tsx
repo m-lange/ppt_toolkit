c7a27824ac6cf5f7b2371c5cb76aa21e48837f55
Martin Lange <mail@martinlange.eu>
2026-06-12 15:33:00 +0200

    Change from tabList to navButton

--- src/components/TemplatesTab.tsx
+++ src/components/TemplatesTab.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { SearchBox, Button, Field } from '@fluentui/react-components';
+import { Button, Field } from '@fluentui/react-components';
 import { ShapesRegular } from '@fluentui/react-icons';
 import { insertBlueRectangle } from '../utils/powerpointApi';
 
 export const TemplatesTab: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-      {/* SearchBox (Platzhalter ohne Logik) */}
-      <Field label="Vorlagen durchsuchen">
-        <SearchBox placeholder="Suche..."/>
-      </Field>
 
-      {/* Action Button */}
       <Field label="Verfügbare Vorlagen" validationMessage="Klicke auf den Button, um eine Form in die aktive Folie einzufügen.">
         <Button
           appearance="primary"
